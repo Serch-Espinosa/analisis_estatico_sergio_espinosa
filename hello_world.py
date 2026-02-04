6457e61249415fbb603026cfb597e90bf15001b1
Serch-Espinosa <checochique@gmail.com>
2026-02-03 23:06:59 -0600

    Create hello_world.py
    feat: add a program that says "Hola mundo!"

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+"""
+Docstring for hello_world
+A simple hello world module.
+"""
+
+
+def hello_world():
+    """
+    Docstring for hello_world
+    """
+    return "Hola mundo!"
+    
+    """A simple function that returns the string "Hello, World!"."""
+    return "Hello, World!"
